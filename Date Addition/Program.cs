0d7f5f05cff157a3b0ea98a73256c6499d49e009
Banee Ishaque K <baneeishaque@gmail.com>
2021-01-29 02:28:15 +0530

    In

---
+++ Date Addition/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Date_Addition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(DateTime.Now.Add(new TimeSpan(2, 0, 0, 0)).ToString("ddd,MMMM dd,yyyy"));
+            Console.WriteLine(DateTime.Parse("02-12-2014").Add(new TimeSpan(30, 0, 0, 0)).ToString("ddd,MMMM dd,yyyy"));
+            Console.ReadKey();
+        }
+    }
+}
